68f79a709a474595accdb68768c65d031e6ad1ec
Areayea <57454823+Areayea@users.noreply.github.com>
2019-11-06 10:32:20 -0700

    Update JonathanMartinezNoelCorrales_02_5.java
    added skeleton

--- JonathanMartinezNoelCorrales_02_5.java
+++ JonathanMartinezNoelCorrales_02_5.java
@@ -1,1 +1,55 @@
 
+
+
+import java.util.Scanner;  //Access Scanner Class
+import java.io.*;          //Access FileWriter
+
+public class JonathanMartinezNoelCorrales_2_05{
+   
+   public static int readData(Employee empl, Scanner input){
+   
+      return 0;
+      } //End method
+      
+      //*************************************
+      
+   public static void calcGrossPay(Employee empl, int numRead){
+      
+      } //End method
+      
+      //*************************************
+      
+   public static void calcDeductions(Employee empl, int numRead){
+   
+      } //End method
+      
+      //*************************************
+      
+   public static void printReport(Employee empl,int nread, PrintWriter output){
+      PrintHeading();
+      PrintDetail();
+      PrintSummary();
+      
+      } //End method
+      
+      //*************************************
+   
+   public static void main(String[] args) throws IOException {
+   
+      File inputDataFile = new File(INPUT_FILE);
+      Scanner inputFile  = new Scanner(inputDataFile);
+      
+      FileWriter outputDataFile = new FileWriter(OUTPUT_FILE);
+      PrintWriter outputFile = new PrintWriter(outputDataFile);
+      
+      Employee empl = new Employee[2];
+         while (inputFile.hasNext()){
+            empl[i] = new Employee();
+            empl[i].hoursWorked = input.nextDouble();
+            empl[i].payRate = input.nextDouble();
+         } //End while
+         
+    } //End main
+    
+} //End class
+            
